aa5660c08752f4316ecb891213d38cf86196ece6
Sahil4883 <shaikhsahil4883@gmail.com>
2024-08-09 21:19:00 +0530

    error page made

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -53,6 +53,6 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
